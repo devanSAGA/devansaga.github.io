05ec145c07dd66db68042edbd797b1bbdf69d26f
Devansh Purohit <officialdevansh@gmail.com>
2019-03-17 18:16:41 +0530

    Updates

---
+++ precache-manifest.98161fe81e8392446bd75a81f0418781.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2529cbc1781fbbcb359941a95b1ef2f6",
+    "url": "/static/media/myAvatar.2529cbc1.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "cdb97229ec23e3491a6b",
+    "url": "/static/js/main.cdb97229.chunk.js"
+  },
+  {
+    "revision": "cf21ecf3561e7f2cef3e",
+    "url": "/static/js/2.cf21ecf3.chunk.js"
+  },
+  {
+    "revision": "cdb97229ec23e3491a6b",
+    "url": "/static/css/main.d47ac9ac.chunk.css"
+  },
+  {
+    "revision": "6267db869d954539289fd9485ee645bb",
+    "url": "/index.html"
+  }
+];
