af3f5d718d039cb3e799e972d78e9277b86c098d
Devansh Purohit <officialdevansh@gmail.com>
2019-03-17 18:26:07 +0530

    Updates

---
+++ precache-manifest.58faf7eca6a9710cb6a9abe4400321c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2529cbc1781fbbcb359941a95b1ef2f6",
+    "url": "/static/media/myAvatar.2529cbc1.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "0581e9f8ae1755e5fe83",
+    "url": "/static/js/main.0581e9f8.chunk.js"
+  },
+  {
+    "revision": "dbea9a99fd4106beab17",
+    "url": "/static/js/2.dbea9a99.chunk.js"
+  },
+  {
+    "revision": "0581e9f8ae1755e5fe83",
+    "url": "/static/css/main.e019b3fc.chunk.css"
+  },
+  {
+    "revision": "fe5ac2c7a5fad73af0db7a8420414cf6",
+    "url": "/index.html"
+  }
+];
