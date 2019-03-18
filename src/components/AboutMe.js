84aa6f56ca82ef98ecc219dd6b29f92ab252c5c1
Devansh Purohit <officialdevansh@gmail.com>
2019-03-18 05:48:33 +0530

    fixed Overlapping in Chrome bug

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,27 +4,21 @@ import "../styles/AboutMe.css";
 const AboutMe = () => {
   return (
     <div className="aboutme-page-container">
-      <section className="aboutme ">
-        Oh you clicked! <span className="half-highlight">Awesome.</span>
+      <section className="aboutme ">Oh you clicked! Awesome.</section>
+      <section className="aboutme">
+        My name is Devansh, I am a junior front end developer and I am from
+        India. Currently, I am pursuing my bachelor of technology in the field
+        of Information and Communication Technology(4th year) at Dhirubhai
+        Ambani Institute of Information and Communication Technology ,
+        Gandhinagar.
       </section>
       <section className="aboutme">
-        My name is <span className="half-highlight">Devansh</span>, I am a
-        junior front end developer and I am from{" "}
-        <span className="half-highlight">India</span>. Currently, I am pursuing
-        my bachelor of technology in the field of Information and Communication
-        Technology(4th year) at Dhirubhai Ambani Institute of Information and
-        Communication Technology , Gandhinagar.
-      </section>
-      <section className="aboutme">
-        Along with <span className="half-highlight">JavaScript</span> I love
-        Cartoons, Superheroes and collecting Nerdy T-Shirts. I am currently
-        experimenting <span className="half-highlight">React</span> and
-        implementing some of my ideas. The other side of me is an Artist. I have
-        been fond of drawing since my childhood and after getting introduced to
-        Adobe Illustrator I try to represent my Ideas through{" "}
-        <span className="half-highlight">Illustrations</span>. I am good at
-        making puns and that is reflected in my Designs too. You can follow me
-        on{" "}
+        Along with JavaScript I love Cartoons, Superheroes and collecting Nerdy
+        T-Shirts. I am currently experimenting React and implementing some of my
+        ideas. The other side of me is an Artist. I have been fond of drawing
+        since my childhood and after getting introduced to Adobe Illustrator I
+        try to represent my Ideas through Illustrations. I am good at making
+        puns and that is reflected in my Designs too. You can check my{" "}
         <a
           href="https://www.instagram.com/_devansaga_/"
           target="_blank"
@@ -33,26 +27,25 @@ const AboutMe = () => {
         >
           Instagram
         </a>{" "}
-        to get more hang of it.
+        to have a look at my designs and know more.
       </section>
       <section className="aboutme">
-        In my spare time, I would be mostly, watching{" "}
-        <span className="half-highlight">Football</span> or participating in a{" "}
-        <span className="half-highlight">Quiz</span> or scrolling on{" "}
+        In my spare time, I watch Football or participate in a Quiz or scroll
+        through{" "}
         <a
           href="https://twitter.com/devanshdoesdab"
           target="_blank"
           rel="noopener noreferrer"
-          style={{ color: "#34699a", padding: 0, fontWeight: "bold" }}
+          style={{ color: "#5893d4", padding: 0, fontWeight: "bold" }}
         >
           Twitter
         </a>{" "}
-        or listening to{" "}
+        or listen to{" "}
         <a
           href="http://www.thelocaltrain.com/"
           target="_blank"
           rel="noopener noreferrer"
-          style={{ padding: 0, fontWeight: "bold" }}
+          style={{ padding: 0, fontWeight: "bold", color: "#113f67" }}
         >
           The Local Train.
         </a>
@@ -61,7 +54,7 @@ const AboutMe = () => {
         If you would like to say hello,
         <br />
         Feel free to Contact me{" "}
-        <span className="half-highlight">@dnpurohit96@gmail.com</span>
+        <span className="bold">@dnpurohit96@gmail.com</span>
       </section>
       <section className="aboutme">Bye!</section>
       <section className="footer" />
