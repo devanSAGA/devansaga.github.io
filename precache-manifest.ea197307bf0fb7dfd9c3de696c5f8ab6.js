8f80ba9d19e8a0b9b51ba02f4621b9cdfe941526
Devansh <officialdevansh@gmail.com>
2019-10-01 12:14:47 +0530

    Updates

---
+++ precache-manifest.ea197307bf0fb7dfd9c3de696c5f8ab6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2529cbc1781fbbcb359941a95b1ef2f6",
+    "url": "/static/media/myAvatar.2529cbc1.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "ced7fe468196fdcd5a14",
+    "url": "/static/js/main.ced7fe46.chunk.js"
+  },
+  {
+    "revision": "626f9cc0e467415c4241",
+    "url": "/static/js/2.626f9cc0.chunk.js"
+  },
+  {
+    "revision": "ced7fe468196fdcd5a14",
+    "url": "/static/css/main.4be79757.chunk.css"
+  },
+  {
+    "revision": "12b3fd7575d412c3083c02cb6717773d",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.17735ab3187c3e2cc93a1b8688656650.js"
+  "/precache-manifest.ea197307bf0fb7dfd9c3de696c5f8ab6.js"
 );
 
 workbox.clientsClaim();
